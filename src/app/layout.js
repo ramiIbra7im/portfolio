0a4042655504356e70e784379882d27cc17b269d
ramiIbra7im <abrahymramy19@gmail.com>
2025-06-23 16:13:14 +0300

    تحديث المشروع بعد التصليحات

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapClient from './components/BootstrapClient';
-// export const metadata = {
-//   title: "portfolio",
-//   description: "Converted from React to Next.js App Router",
-// };
+export const metadata = {
+  title: "portfolio",
+  description: "Ramy Ibrahim portfolio",
+};
 
 export default function RootLayout({ children }) {
   return (
